495923e0a42484c27f2fcbc40d4ab46600d248af
waybig125 <waybig125@gmail.com>
2024-05-21 11:56:28 +0500

    update PostCard.jsx and Postcard.jsx

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import ArrowRight from "./icons/ArrowRight";
+
 import Tag from "./Tag";
 import { Post } from "../lib/types";
 import AuthorAttribution from "./AuthorAttribution";
